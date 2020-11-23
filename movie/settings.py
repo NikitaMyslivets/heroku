f52decccc2b39ab193346d80d6d29e83b185a1d3
Nikita <nikita.myslivets@mail.ru>
2020-11-23 22:13:44 +0300

    try to add css 6

--- movie/settings.py
+++ movie/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'main/static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
